5c954eba6929751ce86fa43b0097dd6c2679c3ab
issaka-sanfo <issaka.sanfo@epita.fr>
2022-11-06 21:45:26 +0100

    Add project files.

---
+++ ChatApp/Server/Hubs/ChatHub.cs
@@ -0,0 +1,29 @@
+﻿using Duende.IdentityServer.EntityFramework.Entities;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Server.Hubs
+{  
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+        public override async Task OnConnectedAsync()
+        {
+            string username = Context.GetHttpContext().Request.Query["username"];
+            //string username = "Issaka";
+            Users.Add(Context.ConnectionId, username);
+            await AddMessageToChat(string.Empty, $"{username} a rejoint le Chat!");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} left!");
+        }
+
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
